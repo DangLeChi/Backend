6b27624aba5a44f4cee88d36d50bec6f88db1bb1
DangLeChi <danglechi5474770306@gmail.com>
2024-03-27 16:05:53 +0700

    add_multi_folder_for_src

--- src/server.js
+++ src/server.js
@@ -5,19 +5,24 @@ const path = require('path')
 require('dotenv').config()
 
 
-console.log(">>check env", process.env)
+// console.log(">>check env", process.env)
 const port =process.env.PORT || 8888;
-console.log(process.env.HOSTNAME)
+// console.log(process.env.HOSTNAME)
 const hostname = process.env.HOSTNAME
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 // config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+// config static files
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Chi')
+})
+
+
+
 
 app.get('/abc', (req, res) => {
   res.send('Check abc')
@@ -32,3 +37,4 @@ app.get('/hoidanit', (req, res) => {
 app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
