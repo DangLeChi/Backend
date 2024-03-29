623bdefbce613ca077150c12b2aca810c7f5c4cd
DangLeChi <danglechi5474770306@gmail.com>
2024-03-29 20:16:46 +0700

    move db on that file and call

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-const mysql = require('mysql2')
+const connection = require('./config/database')
 
 const app = express()
 const port =process.env.PORT || 8888;
@@ -34,21 +34,12 @@ app.get('/hoidanit', (req, res) => {
 
 
 // test connectiôn
-// Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307, // default: 3306
-  user: 'root', //default: empty
-  password : '123456',
-  database: 'hoidanit',
-});
-
 // A simple SELECT query
 connection.query(
   'SELECT * from Users u',
   function (err, results, fields) {
     console.log(">>> result ",results); // results contains rows returned by server
-    console.log(">>>fields ",fields); // fields contains extra meta data about results, if available
+    
   }
 );
 
